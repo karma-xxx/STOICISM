370a4c7c066bb0ee6c69252803913d3970b8be98
luis salcedo <luisfernandos1564@gmail.com>
2026-05-16 02:08:31 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,115 +1,109 @@
-<?php
-$alert = '';
-session_start();
-if (!empty($_SESSION['active'])) {
-  header('location: sistema/');
-} else {
-  if (!empty($_POST)) {
-    if (empty($_POST['usuario']) || empty($_POST['clave'])) {
-      $alert = '<div class="alert alert-danger" role="alert">
-  Ingrese su usuario y su clave
-</div>';
-    } else {
-      require_once "conexion.php";
-      $user = mysqli_real_escape_string($conexion, $_POST['usuario']);
-      $clave = md5(mysqli_real_escape_string($conexion, $_POST['clave']));
-      $query = mysqli_query($conexion, "SELECT u.idusuario, u.nombre, u.correo,u.usuario,r.idrol,r.rol FROM usuario u INNER JOIN rol r ON u.rol = r.idrol WHERE u.usuario = '$user' AND u.clave = '$clave'");
-      mysqli_close($conexion);
-      $resultado = mysqli_num_rows($query);
-      if ($resultado > 0) {
-        $dato = mysqli_fetch_array($query);
-        $_SESSION['active'] = true;
-        $_SESSION['idUser'] = $dato['idusuario'];
-        $_SESSION['nombre'] = $dato['nombre'];
-        $_SESSION['email'] = $dato['correo'];
-        $_SESSION['user'] = $dato['usuario'];
-        $_SESSION['rol'] = $dato['idrol'];
-        $_SESSION['rol_name'] = $dato['rol'];
-        header('location: sistema/');
-      } else {
-        $alert = '<div class="alert alert-danger" role="alert">
-              Usuario o Contraseña Incorrecta
-            </div>';
-        session_destroy();
-      }
-    }
-  }
-}
-?>
-<!DOCTYPE html>
-<html lang="en">
+<?php include_once "includes/header.php"; ?>
 
-<head>
+<div class="container-fluid">
+	<!-- Page Heading -->
+	<div class="d-sm-flex align-items-center justify-content-between mb-4">
+		<h1 class="h3 mb-0 text-gray-800">Panel de Administración</h1>
+	</div>
 
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+	<!-- Content Row -->
+	<div class="row">
 
-  <title>Vida Informatico</title>
+		<!-- Earnings (Monthly) Card Example -->
+		<a class="col-xl-3 col-md-6 mb-4" href="lista_usuarios.php">
+			<div class="card border-left-primary shadow h-100 py-2 bg-white">
+				<div class="card-body">
+					<div class="row no-gutters align-items-center">
+						<div class="col mr-2">
+							<div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Usuarios</div>
+							<div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $data['usuarios']; ?></div>
+						</div>
+						<div class="col-auto">
+							<i class="fas fa-user fa-2x text-gray-300"></i>
+						</div>
+					</div>
+				</div>
+			</div>
+		</a>
 
-  <!-- Custom fonts for this template-->
-  <link rel="stylesheet" href="sistema/vendor/bootstrap/css/bootstrap.min.css">
-  <!-- Custom styles for this template-->
-  <link href="sistema/css/style.violet.css" rel="stylesheet">
+		<!-- Earnings (Monthly) Card Example -->
+		<a class="col-xl-3 col-md-6 mb-4" href="lista_cliente.php">
+			<div class="card border-left-success shadow h-100 py-2 bg-white">
+				<div class="card-body">
+					<div class="row no-gutters align-items-center">
+						<div class="col mr-2">
+							<div class="text-xs font-weight-bold text-success text-uppercase mb-1">Clientes</div>
+							<div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $data['clientes']; ?></div>
+						</div>
+						<div class="col-auto">
+							<i class="fas fa-users fa-2x text-gray-300"></i>
+						</div>
+					</div>
+				</div>
+			</div>
+		</a>
 
-</head>
+		<!-- Earnings (Monthly) Card Example -->
+		<a class="col-xl-3 col-md-6 mb-4" href="lista_productos.php">
+			<div class="card border-left-info shadow h-100 py-2 bg-white">
+				<div class="card-body">
+					<div class="row no-gutters align-items-center">
+						<div class="col mr-2">
+							<div class="text-xs font-weight-bold text-info text-uppercase mb-1">Productos</div>
+							<div class="row no-gutters align-items-center">
+								<div class="col-auto">
+									<div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"><?php echo $data['productos']; ?></div>
+								</div>
+								<div class="col">
+									<div class="progress progress-sm mr-2">
+										<div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-auto">
+							<i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+						</div>
+					</div>
+				</div>
+			</div>
+		</a>
 
-<body class="bg-gradient-primary">
-
-  <div class="container">
-    <!-- Outer Row -->
-    <div class="row justify-content-center">
-
-      <div class="col-xl-10 col-lg-12 col-md-9">
-
-        <div class="card o-hidden border-0 shadow-lg my-5">
-          <div class="card-body p-0">
-            <!-- Nested Row within Card Body -->
-            <div class="row">
-              <div class="col-lg-6 d-none d-lg-block bg-login-image">
-                <img src="sistema/img/logo.jpg" class="img-thumbnail">
-              </div>
-              <div class="col-lg-6">
-                <div class="p-5">
-                  <div class="text-center">
-                    <h1 class="h4 text-gray-900 mb-4">Iniciar Sesión</h1>
-                  </div>
-                  <form class="user" method="POST">
-                    <?php echo isset($alert) ? $alert : ""; ?>
-                    <div class="form-group">
-                      <label for="">Usuario</label>
-                      <input type="text" class="form-control" placeholder="Usuario" name="usuario"></div>
-                    <div class="form-group">
-                      <label for="">Contraseña</label>
-                      <input type="password" class="form-control" placeholder="Contraseña" name="clave">
-                    </div>
-                    <input type="submit" value="Iniciar" class="btn btn-primary">
-                    <hr>
-                  </form>
-                  <hr>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-      </div>
-
-    </div>
-
-  </div>
-
-  <!-- JavaScript files-->
-    <script src="sistema/vendor/jquery/jquery.min.js"></script>
-    <script src="sistema/vendor/popper.js/umd/popper.min.js"> </script>
-    <script src="sistema/vendor/bootstrap/js/bootstrap.min.js"></script>
-    <script src="sistema/vendor/jquery.cookie/jquery.cookie.js"> </script>
-    <script src="sistema/vendor/jquery-validation/jquery.validate.min.js"></script>
-    <script src="sistema/js/front.js"></script>
+		<!-- Pending Requests Card Example -->
+		<a class="col-xl-3 col-md-6 mb-4" href="ventas.php">
+			<div class="card border-left-warning bg-white shadow h-100 py-2">
+				<div class="card-body">
+					<div class="row no-gutters align-items-center">
+						<div class="col mr-2">
+							<div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Ventas</div>
+							<div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $data['ventas']; ?></div>
+						</div>
+						<div class="col-auto">
+							<i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+						</div>
+					</div>
+				</div>
+			</div>
+		</a>
+		<div class="col-lg-6">
+			<div class="au-card m-b-30">
+				<div class="au-card-inner">
+					<h3 class="title-2 m-b-40">Productos con stock mínimo</h3>
+					<canvas id="sales-chart"></canvas>
+				</div>
+			</div>
+		</div>
+		<div class="col-lg-6">
+			<div class="au-card m-b-30">
+				<div class="au-card-inner">
+					<h3 class="title-2 m-b-40">Productos más vendidos</h3>
+					<canvas id="polarChart"></canvas>
+				</div>
+			</div>
+		</div>
+	</div>
 
 
-</body>
+</div>
 
-</html>
+<?php include_once "includes/footer.php"; ?>
